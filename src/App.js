4a5e4940fb131eab2fd06c752ac2a1e46994ad83
Carla <carlamendozadominguez@gmail.com>
2020-05-30 17:58:45 +0200

    First step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//function Hello (props){
+//  return <h2>{props.title}</h2>
+//}
+
+//const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello title='Hello from props'></Hello>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
