b2cd480963b49dd482dff923c604881c0a1a1132
Carla <carlamendozadominguez@gmail.com>
2020-05-30 19:03:16 +0200

    States

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Contador extends Component {
+  state = {contador: this.props.contadorInicial};
+
+  render(){
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 })
+    }, 10000);
+
+    return (
+     <ContadorNumero number = {this.state.contador}/>
+    );
+  }
+}
+
+class ContadorNumero extends Component {
+  render() {
+    return <span>{this.props.number}</span>
+  }
+}
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <p> Mi first component with state</p>
+        <Contador contadorInicial = {2}/>
+      </div>
+    );
+  }
 }
 
 export default App;
