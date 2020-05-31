327fbc692da0c632ef5947ba0309c592a1aec876
Carla <carlamendozadominguez@gmail.com>
2020-05-31 16:33:16 +0200

    Children

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from'prop-types'
+
+class Box extends Component {
+  render() {
+    return (
+      <div style = {{border: '1px solid #09f', margin: 5, padding: 5}}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+
+class Article extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  }
+  render() {
+    const {
+      title,
+      author,
+      date,
+      children
+    } = this.props; //To delete the reference this.props in each varible 
+    return (
+      <section>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
+        <article>
+          {children}
+        </article>
+      </section>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Article
+        date = { new Date().toLocaleDateString()}
+        title = 'Prop children 1'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+          <strong> Y mantiene las etiquetas y componentes que hemos añadido dentro</strong>
+        </Article>
+
+        <Article
+        author = 'Carla Mendoza'
+        date = { new Date().toLocaleDateString()}
+        title = 'Prop children 2'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+          <strong> Y mantiene las etiquetas y componentes que hemos añadido dentro</strong>
+        </Article>
       </div>
     );
   }
